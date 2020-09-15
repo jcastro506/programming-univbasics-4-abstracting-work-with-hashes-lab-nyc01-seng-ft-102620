3fe35e9fd39565b684668f3ff2807d08ed3760cf
breezy-builder-9254 <castrojoshua805@gmail.com>
2020-09-15 01:46:14 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -9,5 +9,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-  hash[key] == 1 
+  if hash[key]
+    return hash[key] + 1 
+  else 
+    key = 1 
+    return hash[key]
+  end 
 end
